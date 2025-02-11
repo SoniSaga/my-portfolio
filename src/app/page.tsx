db1a6f38a480c717a14386be96aaaabb9f2b56c3
Soni Bro <shantanusoni979@gmail.com>
2025-02-12 01:18:09 +0530

    Changing paths

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 import { useContext } from "react";
-import About from "../components/about/About";
-import Contact from "../components/contact/Contact";
-import Intro from "../components/intro/Intro";
-import ProductList from "../components/productList/ProductList";
+import About from "@/components/about/About";
+import Contact from "@/components/contact/Contact";
+import Intro from "@/components/intro/Intro";
+import ProductList from "@/components/productList/ProductList";
 
 
 const Homepage = () => {
